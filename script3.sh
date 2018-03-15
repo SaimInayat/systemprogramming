3d18e77c371b281d028864761f567d26d9f84393
SaimInayat <36986017+SaimInayat@users.noreply.github.com>
2018-03-15 21:34:37 +0500

    Add files via upload

---
+++ script3.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "Task 3 Array Manipulation"
+
+echo "Array contains the following elements:"
+UNIX=(Debian Redhat Ubuntu Suse Fedora)
+echo ${UNIX[*]}
+
+echo "Total number of elements in array equal: "
+echo ${#UNIX[*]}
+
+echo "Length of element on index 2 is: "
+echo ${#UNIX[2]}
+
+echo "Extracting 2 elements from index 3 onwards"
+unset UNIX[3]
+unset UNIX[4]
+
+echo ${UNIX[*]}
+
+echo "Now replacing Ubuntu with SCO Unix"
+echo ${UNIX[@]/2/SCOUnix}
+
+echo "Now adding elements AIX and HP-UX in the array"
+echo ${UNIX[@]AIX HP AUX}
+
+echo "After removing 3rd element from array UNIX"
+unset ${UNIX[2]}
+
+echo "After removing array LINUX"
+unset LINUX
+
+echo "After removing array UNIX"
+unset UNIX
+
+
